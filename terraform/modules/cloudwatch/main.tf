14699ff33c65ed5345af0fbb741ee2920fd6c342
YeeshaDev <aishaagunbiade05@gmail.com>
2026-06-22 15:46:54 +0100

    feat: increase CloudWatch log retention from 7 to 14 days

--- terraform/modules/cloudwatch/main.tf
+++ terraform/modules/cloudwatch/main.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "aws_cloudwatch_log_group" "eks_cluster" {
   name              = "/aws/eks/${var.cluster_name}/cluster"
-  retention_in_days = 7
+  retention_in_days = 14
 
   tags = {
     Project = "karatu-2025-capstone"
@@ -13,7 +13,7 @@ resource "aws_cloudwatch_log_group" "eks_cluster" {
 
 resource "aws_cloudwatch_log_group" "app" {
   name              = "/aws/eks/${var.cluster_name}/retail-app"
-  retention_in_days = 7
+  retention_in_days = 14
 
   tags = {
     Project = "karatu-2025-capstone"
